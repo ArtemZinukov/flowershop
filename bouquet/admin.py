929c35c8691331a65a9e98d7b388a23e677856de
bfc0 <bfcheg@gmail.com>
2024-09-18 17:30:32 +0300

    Added model for bouquet

--- bouquet/admin.py
+++ bouquet/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from django.contrib import admin
+from .models import Bouquet, Event
+
+
+class BouquetAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "status", "image_preview")
+    list_filter = ("status",)
+    search_fields = ("title", "composition_description")
+    fields = ("title", "price", "status", "composition_description",
+              "image", "image_preview", "size_description", "events")
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="150" height="150" />')
+        return "No Image"
+
+    image_preview.short_description = "Image Preview"
+
+
+admin.site.register(Bouquet, BouquetAdmin)
+admin.site.register(Event)
--- bouquet/models.py
+++ bouquet/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100, blank=False,
+                             null=False, verbose_name="Название")
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Bouquet(models.Model):
+    STATUS = (
+        ("NEW", "Новый"),
+        ("PAY", "Оплачен"),
+        ("CFM", "Подтвержден"),
+        ("COM", "Завершен"),
+    )
+    title = models.CharField(max_length=100, blank=False,
+                             null=False, verbose_name="Название")
+    price = models.DecimalField(
+        blank=False, null=False, max_digits=8, decimal_places=2, verbose_name="Цена")
+    status = models.CharField(
+        max_length=3, choices=STATUS, default="NEW", verbose_name="Статус", db_index=True)
+    composition_description = models.TextField(
+        blank=False, null=False, verbose_name="Состав")
+    image = models.ImageField(upload_to="images/")
+    size_description = models.CharField(
+        max_length=100, blank=False, null=False, verbose_name="Размер")
+    events = models.ManyToManyField(Event, blank=True)
+
+    def __str__(self):
+        return str(self.title)
